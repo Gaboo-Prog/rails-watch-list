938b9b63f1340a888b47e6f6f0197bf91f7a1634
gaboo <sgaboo@yahoo.com>
2021-08-12 13:17:02 -0400

    listado

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,21 @@
 class BookmarksController < ApplicationController
+  def new
+    @movies = Movie.new
+  end
+  
+  def create
+    @task = Task.new(movie_params)
+    
+      if @task.save
+        redirect_to task_path(@task)
+      else
+        render :new
+    end
+    
+    def update
+    
+    end
+  end 
+
+
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,31 @@
 class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+  
+  def create
+    @list = List.new(list_params)
+      if @list.save
+        redirect_to list_path(@list)
+      else
+        render :new
+    end
+    
+    def update
+    
+    end
+  end 
+  # def show
+  #   @lists = list.find(params[:id])
+  # end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
