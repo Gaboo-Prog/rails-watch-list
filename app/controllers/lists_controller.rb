c1c932c5e9990eefac4b2db5ca78c8faa3c330e8
gaboo <sgaboo@yahoo.com>
2021-08-13 11:28:29 -0400

    avance

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,27 +3,33 @@ class ListsController < ApplicationController
     @lists = List.all
   end
 
+  def show
+    @bookmark = Bookmark.new
+  end
+
   def new
     @list = List.new
   end
   
   def create
     @list = List.new(list_params)
-      if @list.save
-        redirect_to list_path(@list)
-      else
-        render :new
-    end
-    
-    def update
-    
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
     end
   end 
-  # def show
-  #   @lists = list.find(params[:id])
-  # end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path
+  end
 
   private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
 
   def list_params
     params.require(:list).permit(:name)
